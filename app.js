a0c5c39640aaba975db34bbebfc53db3c9de9355
CuCaRot <namkienlua@gmail.com>
2021-09-20 19:27:43 +0700

    2nd commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
 }); 
 app.use('client', express.static(__dirname + '/client'));
 
-server.listen(2000);
+server.listen(process.env.PORT || 2000);
 console.log('Server started');
 
 const SOCKET_LIST = {};
